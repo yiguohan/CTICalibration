38d3812f214cfc89bb1f67b561c7fbd8784397d6
yiguohan <yiguohan@gmail.com>
2017-04-26 14:48:34 +0800

    添加了下拉刷新显示打卡记录更新

--- app/src/main/java/com/cti_cert/cticalibration/Utils/DataUtil.java
+++ app/src/main/java/com/cti_cert/cticalibration/Utils/DataUtil.java
@@ -1,13 +1,10 @@
 package com.cti_cert.cticalibration.Utils;
 
 import com.cti_cert.cticalibration.Beans.Attendance;
-
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Timer;
+
 
 /**
  * Created by yiguo on 2017/4/25.
@@ -16,14 +13,21 @@ import java.util.Timer;
 
 public class DataUtil {
 
-    public static List<Attendance> initAttendanceData() {
-        List<Attendance> list = new ArrayList<Attendance>();
+    public static void initAttendanceData(List<Attendance> list) {
         Calendar calendar = Calendar.getInstance();
         String date = calendar.get(Calendar.YEAR) + "年" + (calendar.get(Calendar.MONTH) + 1) + "月" + calendar.get(Calendar.DATE) + "日";
         String time = calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND);
-        for (int i = 0; i < 50; i++) {
-            list.add(new Attendance(date, time, "上班打卡"));
+        String type = "上班打卡";
+        if (list.size() > 0) {
+            list.clear();
         }
-        return list;
+        for (int i = 0; i < 50; i++) {
+            list.add(new Attendance(date, time, type));
+            if (type.equals("上班打卡")) {
+                type = "下班打卡";
+            } else {
+                type = "上班打卡";
+            }
+        }
     }
 }
