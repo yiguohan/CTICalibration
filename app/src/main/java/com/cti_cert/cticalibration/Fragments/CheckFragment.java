37abfb437222bb942dd772c05c57c9aa1d9c7b79
yiguohan <yiguohan@gmail.com>
2017-04-26 17:10:59 +0800

    考勤打卡fragment添加了高德地图

--- app/src/main/java/com/cti_cert/cticalibration/Fragments/CheckFragment.java
+++ app/src/main/java/com/cti_cert/cticalibration/Fragments/CheckFragment.java
@@ -2,11 +2,14 @@ package com.cti_cert.cticalibration.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.SupportMapFragment;
 import com.cti_cert.cticalibration.R;
 
 
@@ -15,17 +18,42 @@ import com.cti_cert.cticalibration.R;
  */
 public class CheckFragment extends Fragment {
 
+    private AMap aMap;
+
+    private SupportMapFragment mapFragment;
+
 
     public CheckFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check, container, false);
+        View view = inflater.inflate(R.layout.fragment_check, container, false);
+        setUpMap();
+        return view;
+    }
+
+    /*释放mapFragment。需要先从父级fragment中移除，否则重新加载onCreateView时会抛异常*/
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mapFragment != null) {
+            getChildFragmentManager().beginTransaction().remove(mapFragment).commitAllowingStateLoss();
+        }
+        mapFragment = null;
+        aMap = null;
+    }
+
+    /*初始化AMAP对象*/
+    private void setUpMap() {
+        if (mapFragment == null) {
+            mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        }
+        if (aMap == null) {
+            aMap = mapFragment.getMap();
+        }
     }
 
 }
